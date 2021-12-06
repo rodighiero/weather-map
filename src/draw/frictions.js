3828e427f237ecdb13fd8bef39df0c188d9e0535
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2021-12-06 14:22:49 +0100

    Update frictions.js

--- src/draw/frictions.js
+++ src/draw/frictions.js
@@ -23,15 +23,32 @@ export default entities => {
             const p1 = polygonHull(coo1)
             const p2 = polygonHull(coo2)
 
+            const center_1 = polygonCentroid(p1)
+            const center_2 = polygonCentroid(p2)
+
             let overlapping = false
 
             coo2.forEach(point => {
-                // console.log(c1)
                 if (polygonContains(p1, point))
                     overlapping = true
             })
 
-            console.log(overlapping)
+            if (overlapping) {
+
+                const circle_1 = new Graphics()
+                circle_1.beginFill('0x000000', 1)
+                circle_1.drawCircle(0, 0, 5)
+                circle_1.endFill()
+                circle_1.position = new Point(center_1[0], center_1[1])
+                stage.addChild(circle_1)
+
+                const circle_2 = new Graphics()
+                circle_2.beginFill('0x000000', 1)
+                circle_2.drawCircle(0, 0, 5)
+                circle_2.endFill()
+                circle_2.position = new Point(center_2[0], center_2[1])
+                stage.addChild(circle_2)
+            }
 
         })
     })

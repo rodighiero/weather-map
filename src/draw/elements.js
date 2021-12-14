32bf954c574a767009f083dd8b504b004fb698e1
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2021-12-15 00:42:07 +0100

    Showing all the keywords

--- src/draw/elements.js
+++ src/draw/elements.js
@@ -22,9 +22,9 @@ export default (entities) => {
         // .filter(e => e['type'] !== 'subject') // Keep nodes
         .forEach(e => {
 
-            const isSubject = e['type'] == 'subject'
+            const isSubject = e.type == 'subject'
 
-            const color = isSubject ? s.gray : e['slope'] > 0 ? s.red : s.blue
+            const color = isSubject ? s.gray : e.slope > 0 ? s.red : s.blue
 
             // Cross
 
@@ -45,7 +45,7 @@ export default (entities) => {
             // Label
 
             const bitmap = new BitmapText(
-                splitInTwo(e['name']),
+                splitInTwo(e.name),
                 {
                     fontName: 'Lato',
                     fontSize: 2,
